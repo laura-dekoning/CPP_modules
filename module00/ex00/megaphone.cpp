ffda8799b133562368de8e7a11ad18dbc8ec7639
Laura de Koning <laura-dekoning@live.nl>
2025-06-01 18:27:06 +0200

    ex00 made and checked

---
+++ module00/ex00/megaphone.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   megaphone.cpp                                      :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: lade-kon <lade-kon@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2025/06/01 16:38:16 by lade-kon      #+#    #+#                 */
+/*   Updated: 2025/06/01 18:21:09 by lade-kon      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <cctype>
+
+int	main(int argc, char **argv)
+{
+	if (argc > 1)
+	{
+		for (int i = 1; argv[i] != NULL; i++)
+		{
+			for (int j = 0; argv[i][j] != '\0'; j++)
+				std::cout << static_cast<char>(std::toupper(static_cast<unsigned char>(argv[i][j])));
+		}
+		std::cout << std::endl;
+	}
+	else
+		std::cout << "* LOUD AND UBEARABLE FEEDBACK NOISE *" << std::endl;
+	return 0;
+}
