449c51bc273e5e0f4b07552e26566089ffc98a58
Laura de Koning <lade-kon@student.codam.nl>
2025-07-15 15:10:37 +0200

    Finished main.cpp of module01/ex06

--- module01/ex06/src/main.cpp
+++ module01/ex06/src/main.cpp
@@ -6,14 +6,21 @@
 /*   By: lade-kon <lade-kon@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/07/14 15:54:51 by lade-kon      #+#    #+#                 */
-/*   Updated: 2025/07/14 17:36:29 by lade-kon      ########   odam.nl         */
+/*   Updated: 2025/07/15 15:10:14 by lade-kon      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "HarlFilter.hpp"
+#include "Harl.hpp"
 
 int	main(int argc, char **argv){
+	Harl	Kevin;
 
+	if (argc != 2){
+		std::cout << "Incorrect format\nFormat: ./harlFilter <string>" << std::endl;
+		return 1;
+	}
+	else
+		Kevin.complain(argv[1]);
 	
 	return 0;
 }
