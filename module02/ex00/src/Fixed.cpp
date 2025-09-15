7146cbf8bc9b0304dfd4f405da0cc18621b77ffd
Laura de Koning <lade-kon@student.codam.nl>
2025-09-15 11:33:15 +0200

    Finished module02/ex00

--- module02/ex00/src/Fixed.cpp
+++ module02/ex00/src/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: lade-kon <lade-kon@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/09/01 14:27:52 by lade-kon      #+#    #+#                 */
-/*   Updated: 2025/09/01 17:26:41 by lade-kon      ########   odam.nl         */
+/*   Updated: 2025/09/15 11:21:52 by lade-kon      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,25 +18,28 @@ Fixed::Fixed() : _fixedPointValue(0){
 	std::cout << "Default constructor called" << std::endl;
 }
 
-Fixed::Fixed(const Fixed& value){
+/* Is called when a new object is created from an existing one. */
+Fixed::Fixed(const Fixed& other) : _fixedPointValue(other._fixedPointValue) {
 	std::cout << "Copy constructor called" << std::endl;
-
-
 }
 
-Fixed&	Fixed::operator=(Fixed& value){
+/* Is called when an already existing object is assigned a VALUE of another existing object. Defines how objects behave when assignment operator (=) is used. Check for not trying to copy itself. Creating deep copy of other. */
+Fixed&	Fixed::operator=(Fixed& other){
 	std::cout << "Copy assignment operator called" << std::endl;
-
-
+	if (this != &other)
+		_fixedPointValue = other._fixedPointValue;
+	return *this;
 }
 
 Fixed::~Fixed(){
 	std::cout << "Destructor called" << std::endl;
 }
 
+/* Getters & Setters */
+
 int		Fixed::getRawBits(void)const{
 	std::cout << "getRawBits member function called" << std::endl;
-	return (_fixedPointValue);
+	return _fixedPointValue;
 }
 
 void	Fixed::setRawBits(int const raw){
--- module02/ex00/src/main.cpp
+++ module02/ex00/src/main.cpp
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   main.c                                             :+:    :+:            */
+/*   main.cpp                                           :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: lade-kon <lade-kon@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/09/01 14:24:52 by lade-kon      #+#    #+#                 */
-/*   Updated: 2025/09/01 17:25:20 by lade-kon      ########   odam.nl         */
+/*   Updated: 2025/09/15 11:32:34 by lade-kon      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,5 +23,20 @@ int main(void){
 	std::cout << a.getRawBits() << std::endl;
 	std::cout << b.getRawBits() << std::endl;
 	std::cout << c.getRawBits() << std::endl;
+
+/* Own tests */
+	a.setRawBits(12);
+	b.setRawBits(10);
+	c.setRawBits(120240360);
+
+	std::cout << a.getRawBits() << std::endl;
+	std::cout << b.getRawBits() << std::endl;
+	std::cout << c.getRawBits() << std::endl;
+	
+	a = c;
+	
+	std::cout << a.getRawBits() << std::endl;
+	std::cout << c.getRawBits() << std::endl;
+
 	return 0;
 }
