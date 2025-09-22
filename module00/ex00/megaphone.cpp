da23060012fe2528a03615e07a4b82abff8e2ee1
Laura de Koning <lade-kon@student.codam.nl>
2025-09-22 16:52:02 +0200

    Added some documentation on ex00

--- module00/ex00/megaphone.cpp
+++ module00/ex00/megaphone.cpp
@@ -6,13 +6,18 @@
 /*   By: lade-kon <lade-kon@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/06/01 16:38:16 by lade-kon      #+#    #+#                 */
-/*   Updated: 2025/09/08 13:02:26 by lade-kon      ########   odam.nl         */
+/*   Updated: 2025/09/22 16:51:23 by lade-kon      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <cctype>
 
+/**
+ * int toupper(int c); takes an int and not a char. if char is passed it could invoke 
+ * undefined behavior if a signed char was used and ASCII values become negative.
+ * return is an int as well. printing with std::cout requires a char cast. 
+ */
 int	main(int argc, char **argv)
 {
 	if (argc > 1)
