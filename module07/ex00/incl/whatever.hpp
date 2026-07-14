7b24786740bbcb64109aac3e63ea6034ce232343
Laura de Koning <lade-kon@student.codam.nl>
2026-07-14 18:48:13 +0200

    Started cpp07

---
+++ module07/ex00/incl/whatever.hpp
@@ -0,0 +1,21 @@
+#ifndef WHATEVER_HPP
+#define WHATEVER_HPP
+
+
+template	<typename T>
+void	swap(T& first, T& second){
+	
+}
+
+template	<typename T>
+T	min(T const& first, T const& second){
+	
+}
+
+template	<typename T>
+T	max(T const& first, T const& second){
+	
+}
+
+
+#endif
